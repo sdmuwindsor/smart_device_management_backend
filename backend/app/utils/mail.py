4499b1e0a2dc1758cbc1e36e7f9ed3a1d46be10f
Ubuntu <sdm_dev@sdm-vm.01l5ko0g5upe3idue4t0pj3ffe.bx.internal.cloudapp.net>
2022-11-16 17:19:04 -0500

    anamoly detection report sent by email code added in utils

--- backend/app/utils/mail.py
+++ backend/app/utils/mail.py
@@ -8,16 +8,19 @@ class send_mails():
         outlook.send(
             receivers=[to_email],
             subject="Welcome to Smart Device Management",
-            text="Hi "+name+"\n\nUnnovate is happy to welcome you to the smart device management solutions.\nHope your journey with us is fruitful.\n\nThank You\nSDM Team"
+            text="Hi "+name.title()+"\n\nUnnovate is happy to welcome you to the smart device management solutions.\nHope your journey with us is fruitful.\n\nThank You\nSDM Team"
         )
 
 
-    def send_report(name,to_email,file_path):
+    def send_report(name, to_email, file_path, device_name, room_name):
         outlook.username = "sdm.mac.2022@outlook.com"
         outlook.password = "kYDLu6bs2Wz3jcG"
-        #now = string(datetime.datetime.now()).split('.')[0]
+
         outlook.send(
             receivers=[to_email],
-            subject="Anamoly Detection Report",
-            text="Hi "+name+"\n\nUnnovate is happy to welcome you to the smart device management solutions.\nHope your journey with us is fruitful.\n\nThank You\nSDM Team"
+            subject=f"Alert! Anamoly Detected",
+            text="Hi "+name.title()+"\n\nThis is an auto generated email.\nThis email is sent to report an anamoly detected by our system for your smart device '"+device_name+"' in '"+room_name+"' room.\nPFA, the anamoly detection report.\n\nThank You\nSDM Team",
+            attachments={
+                'anamoly.pdf':Path(file_path)
+            }
         )
