33fe39cc16d0c3c2b90462ad2caa12c566e04474
patel8t5 <patel8t5@uwindsor.ca>
2022-11-30 18:23:18 -0500

    usage and power consumption api logic development

--- backend/app/crud/crud_light.py
+++ backend/app/crud/crud_light.py
@@ -1,7 +1,9 @@
 from typing import Any, Dict, Optional
 from datetime import datetime
 from sqlalchemy.orm import Session
-
+from sqlalchemy import func
+import numpy as np
+import random
 from app.crud.base import CRUDBase
 from app.models import Lights, Devices
 from app.schemas.light import Light, LightBase, LightCreate, LightUpdate
@@ -9,7 +11,12 @@ from app.schemas.light import Light, LightBase, LightCreate, LightUpdate
 
 class CRUDLight(CRUDBase[Lights, LightCreate, LightUpdate]):
     def get_brightness_by_dates(self, db: Session, *, start_date: datetime, end_date: datetime, device_id: int) -> Optional[Lights]:
-        return db.query(Lights).filter(Lights.created.between(start_date, end_date),Lights.device_id==device_id).all()
+        data = db.query(Lights).filter(Lights.created.between(start_date, end_date),Lights.device_id==device_id,Lights.brightness!=0).all()
+        df = pd.DataFrame.from_records(data)
+        df = df['created','brightness']
+        df['created'] = df['created'].apply(lambda x: str(x).split(' ')[0])
+        df = df.groupby('created').agg({'brightness':np.mean}).reset_index()
+        return df.to_dict('records')
 
     def get_power_consumption_by_dates(self, db: Session, *, start_date: datetime, end_date: datetime, device_id: int) -> Optional[Lights]:
         data = db.query(Lights).filter(Lights.created.between(start_date, end_date),Lights.device_id==device_id).all()
@@ -20,9 +27,9 @@ class CRUDLight(CRUDBase[Lights, LightCreate, LightUpdate]):
         df['next_time'] = df[Lights.created].shift(-1)
         df['power_consumption'] = df.next_time - df[Lights.created] / pd.Timedelta(hours=1)
         df = df[df[Lights.brightness]==1]
-        df = df.groupby("date").agg({'power_consumption':'sum'})
+        df = df.groupby("date").agg({'power_consumption':'sum'}).reset_index()
         power_data = db.query(Devices.power_rating).filter(Devices.id==device_id).first()
-        df['power_consumption'] = df['power_consumption']*power_data['power_rating']
+        df['power_consumption'] = df['power_consumption']*(power_data['power_rating']+power_data['power_rating']*random.uniform(-0.1,0.1))
         return df.to_dict('records')
 
 light = CRUDLight(Lights)
