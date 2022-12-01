2ed6a57cd6430fd798307f7c2b78ec3efe135533
Ubuntu <sdm_dev@sdm-vm.01l5ko0g5upe3idue4t0pj3ffe.bx.internal.cloudapp.net>
2022-11-30 19:26:22 -0500

    usage report errors

--- backend/app/crud/crud_light.py
+++ backend/app/crud/crud_light.py
@@ -7,13 +7,15 @@ import random
 from app.crud.base import CRUDBase
 from app.models import Lights, Devices
 from app.schemas.light import Light, LightBase, LightCreate, LightUpdate
-
+import pandas as pd
 
 class CRUDLight(CRUDBase[Lights, LightCreate, LightUpdate]):
     def get_brightness_by_dates(self, db: Session, *, start_date: datetime, end_date: datetime, device_id: int) -> Optional[Lights]:
         data = db.query(Lights).filter(Lights.created.between(start_date, end_date),Lights.device_id==device_id,Lights.brightness!=0).all()
+        data = [i.__dict__ for i in data]
+        print(data)
         df = pd.DataFrame.from_records(data)
-        df = df['created','brightness']
+        df = df[['created','brightness']]
         df['created'] = df['created'].apply(lambda x: str(x).split(' ')[0])
         df = df.groupby('created').agg({'brightness':np.mean}).reset_index()
         return df.to_dict('records')
--- backend/app/crud/crud_thermostat.py
+++ backend/app/crud/crud_thermostat.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, Optional
 from datetime import datetime
 from sqlalchemy.orm import Session
-
+import pandas as pd
 from app.crud.base import CRUDBase
 from app.models import Thermostats, Devices
 from app.schemas.thermostat import Thermostat, ThermostatBase, ThermostatCreate, ThermostatUpdate
@@ -12,11 +12,11 @@ class CRUDThermostat(CRUDBase[Thermostats, ThermostatCreate, ThermostatUpdate]):
         data =  db.query(Thermostats).filter(Thermostats.created.between(start_date, end_date),Thermostats.device_id==device_id).all()
         df = pd.DataFrame.from_records(data)
         if 'inside' in type:
-            df = df['created','inside_temperature']
+            df = df[['created','inside_temperature']]
             df['created'] = df['created'].apply(lambda x: str(x).split(' ')[0])
             df = df.groupby('created').agg({'inside_temperature':np.mean}).reset_index()
         else:
-            df = df['created','outside_temperature']
+            df = df[['created','outside_temperature']]
             df['created'] = df['created'].apply(lambda x: str(x).split(' ')[0])
             df = df.groupby('created').agg({'outside_temperature':np.mean}).reset_index()
         return df.to_dict('records')
@@ -24,7 +24,7 @@ class CRUDThermostat(CRUDBase[Thermostats, ThermostatCreate, ThermostatUpdate]):
     def get_humidity_by_dates(self, db: Session, *, start_date: datetime, end_date: datetime, device_id: int) -> Optional[Thermostats]:
         data =  db.query(Thermostats).filter(Thermostats.created.between(start_date, end_date),Thermostats.device_id==device_id).all()
         df = pd.DataFrame.from_records(data)
-        df = df['created','humidity']
+        df = df[['created','humidity']]
         df['created'] = df['created'].apply(lambda x: str(x).split(' ')[0])
         df = df.groupby('created').agg({'humidity':np.mean}).reset_index()
         return df.to_dict('records')
@@ -32,7 +32,7 @@ class CRUDThermostat(CRUDBase[Thermostats, ThermostatCreate, ThermostatUpdate]):
     def get_power_consumption_by_dates(self, db: Session, *, start_date: datetime, end_date: datetime, device_id: int) -> Optional[Thermostats]:
         data = db.query(Thermostats).filter(Thermostats.created.between(start_date, end_date),Thermostats.device_id==device_id).all()
         df = pd.DataFrame.from_records(data)
-        df = df['created','inside_temperature']
+        df = df[['created','inside_temperature']]
         df = df.groupby('created').agg({'inside_temperature':'count'}).reset_index()
         df.rename(columns={'inside_temperature':'power_consumption'},inplace=True)
         df['power_consumption'] = df['power_consumption'].apply(lambda x: x*24/150)
